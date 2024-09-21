72a74b9d0a134ed136208316b715958968df781f
Lashi <lashi0812@gmail.com>
2024-09-21 03:48:07 +0000

    controlled text input

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const charCount = MAX_CHARACTERS - text.length;
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value;
+    if (newText.length > MAX_CHARACTERS) {
+      return;
+    }
+    setText(event.target.value);
+  };
+
   return (
     <form className="form">
       <textarea
+        onChange={handleChange}
+        value={text}
         id="feedback-textarea"
         placeholder="blabla"
         spellCheck={false}
@@ -10,7 +26,7 @@ export default function FeedbackForm() {
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
